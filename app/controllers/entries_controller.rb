0fd40604e9f861c134937b848579dc12b75b71c0
Andy Leonard <A.Leonard90@gmail.com>
2015-01-11 17:46:37 -0500

    initial commit

---
+++ app/controllers/entries_controller.rb
@@ -0,0 +1,32 @@
+class EntriesController < ApplicationController
+	def index
+		@entries = Entry.all
+	end
+
+	def show
+		
+	end 
+
+	def new
+		@entry = Entry.new
+	end
+
+	def create
+		@entry = Entry.new(entry_params)
+	end
+
+	def edit
+		@entry = Entry.find(params[:id])
+	end 
+
+	def update
+	end
+
+	def destroy
+		@entry.destroy
+	end 
+	
+	private
+		def entry_params
+		params.require(:entry).permit(:author, :photo_url, :date_taken)
+end
